91eac3542da735e08effb56abad662d6a93cd6af
jinchiim <nellroll@naver.com>
2022-12-18 18:37:10 +0900

    first commit

---
+++ blogapi/home/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from accounts.models import User
+
+class Blog(models.Model):
+    id = models.AutoField(primary_key=True, null=False, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    blog_text = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.title
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='comment')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_text = models.CharField(max_length=500)
+    
+    def __str__(self) -> str:
+        return self.comment_text
